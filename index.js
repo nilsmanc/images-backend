5d87e009dbd48023ea1f9d9ac64803da4aadd4e0
nilsmanc <etereaiberia@gmail.com>
2022-11-01 13:26:31 +0300

    create user

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import { validationResult } from 'express-validator'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
 import { registerValidaton } from './validations/auth.js'
 import { mongodbLink } from './variables.js'
-import { validationResult } from 'express-validator'
+import UserModel from './models/User.js'
 
 mongoose
   .connect(mongodbLink)
@@ -13,13 +17,48 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/register', registerValidaton, (req, res) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array())
-  }
+app.post('/auth/register', registerValidaton, async (req, res) => {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array())
+    }
 
-  res.json({ success: true })
+    const password = req.body.password
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    })
+
+    const user = await doc.save()
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret',
+      {
+        expiresIn: '30d',
+      },
+    )
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json({
+      ...userData,
+      token,
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'Failed to register',
+    })
+  }
 })
 
 app.listen(4444, (err) => {
