348ba45b3c64c9a5e908af777c882008a0318c41
nilsmanc <etereaiberia@gmail.com>
2022-11-02 10:11:47 +0300

    image upload

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import multer from 'multer'
 
 import { registerValidaton, loginValidaton, postCreateValidation } from './validations.js'
 import { mongodbLink } from './variables.js'
-import checkAuth from './utils/checkAuth.js'
 
-import * as UserController from './controllers/UserController.js'
-import * as PostController from './controllers/PostController.js'
+import { handleValidationErrors, checkAuth } from './utils/index.js'
+
+import { UserController, PostController } from './controllers/index.js'
 
 mongoose
   .connect(mongodbLink)
@@ -15,17 +16,35 @@ mongoose
 
 const app = express()
 
-app.use(express.json())
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname)
+  },
+})
 
-app.post('/auth/login', loginValidaton, UserController.login)
-app.post('/auth/register', registerValidaton, UserController.register)
+const upload = multer({ storage })
+
+app.use(express.json())
+app.use('/uploads', express.static('uploads'))
+
+app.post('/auth/login', loginValidaton, handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidaton, handleValidationErrors, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  })
+})
 
 app.get('/posts', PostController.getAll)
 app.get('/posts/:id', PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidation, PostController.create)
 app.delete('/posts/:id', checkAuth, PostController.remove)
-app.patch('/posts/:id', checkAuth, PostController.update)
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update)
 
 app.listen(4444, (err) => {
   if (err) {
