cd7925168bdfd42574a46d294c273a1649c131c6
nilsmanc <etereaiberia@gmail.com>
2023-02-25 06:46:28 +0300

    typescript

--- index.ts
+++ index.ts
@@ -1,30 +1,31 @@
-import express from 'express'
+import express, { Application } from 'express'
 import mongoose from 'mongoose'
 import fs from 'fs'
 import multer from 'multer'
 import cors from 'cors'
 
 import { registerValidaton, loginValidaton, postCreateValidation } from './validations.js'
-
 import { handleValidationErrors, checkAuth } from './utils/index.js'
-
 import { UserController, PostController, CommentController } from './controllers/index.js'
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI as string)
   .then(() => console.log('Database OK'))
   .catch((err) => console.log('Database error', err))
 
-const app = express()
+const app: Application = express()
+
+type DestinationCallback = (error: Error | null, destination: string) => void
+type FileNameCallback = (error: Error | null, filename: string) => void
 
 const storage = multer.diskStorage({
-  destination: (_, __, cb) => {
+  destination: (_: Express.Request, __: Express.Multer.File, cb: DestinationCallback): void => {
     if (!fs.existsSync('uploads')) {
       fs.mkdirSync('uploads')
     }
     cb(null, 'uploads')
   },
-  filename: (_, file, cb) => {
+  filename: (_: Express.Request, file: Express.Multer.File, cb: FileNameCallback) => {
     cb(null, file.originalname)
   },
 })
@@ -38,6 +39,7 @@ app.use('/uploads', express.static('uploads'))
 
 app.post('/auth/login', loginValidaton, handleValidationErrors, UserController.login)
 app.post('/auth/register', registerValidaton, handleValidationErrors, UserController.register)
+//@ts-ignore
 app.get('/auth/me', checkAuth, UserController.getMe)
 
 app.get('/users', UserController.getAllUsers)
@@ -45,7 +47,7 @@ app.get('/users/:id', UserController.getOneUser)
 
 app.post('/upload', upload.single('image'), (req, res) => {
   res.json({
-    url: `/uploads/${req.file.originalname}`,
+    url: `/uploads/${req.file?.originalname}`,
   })
 })
 
@@ -55,10 +57,13 @@ app.get('/posts', PostController.getAll)
 app.get('/posts/tags', PostController.getLastTags)
 app.get('/posts/:id', PostController.getOne)
 app.get('/posts/user/:id', PostController.getUserPosts)
+//@ts-ignore
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
+//@ts-ignore
 app.delete('/posts/:id', checkAuth, PostController.remove)
 app.patch(
   '/posts/:id',
+  //@ts-ignore
   checkAuth,
   postCreateValidation,
   handleValidationErrors,
@@ -66,12 +71,11 @@ app.patch(
 )
 
 app.get('/comments/:id', CommentController.getPostComments)
+//@ts-ignore
 app.post('/comments', checkAuth, CommentController.create)
+//@ts-ignore
 app.delete('/comments/:id', checkAuth, CommentController.remove)
 
-app.listen(process.env.PORT || 4444, (err) => {
-  if (err) {
-    return console.log(err)
-  }
+app.listen(process.env.PORT || 4444, () => {
   console.log('Server OK')
 })
